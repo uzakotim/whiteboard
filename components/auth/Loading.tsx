1c9febbb65be09231ed9047ce2c16220cf74c938
uzakotim <timauzakov@gmail.com>
2025-10-04 14:51:17 +0500

    more nit fixes

--- components/auth/Loading.tsx
+++ components/auth/Loading.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import Image from 'next/image'
 
-function Loading() {
+function Loading(): React.JSX.Element {
   return (
     <div className='w-full h-full flex flex-col justify-center items-center'>
-        <Image src="/logo.png" alt="Logo" width={120} height={120} className='animate-pulse duration-700 rounded-2xl' />
+        <Image src="/logo.png" alt="Logo" width={120} height={120} className='animate-pulse rounded-2xl' />
     </div>
   )
 }
